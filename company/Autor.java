5c630f80849c7f9efe836469045ea9aaf2411645
Arsene Marinel <marinel.arsene@my.fmi.unibuc.ro>
2021-03-31 04:42:14 +0300

    Etapa I

---
+++ company/Autor.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Autor extends Persoana {
+    private String categorie;
+    private final String nationalitate;
+
+    Autor(){
+        this.categorie = "";
+        this.nationalitate = "";
+    }
+
+    Autor(String nume, String prenume, String categorie, String nationalitate){
+        super(nume, prenume);
+        this.categorie = categorie;
+        this.nationalitate = nationalitate;
+    }
+
+    public void setCategorie(String categorie) { this.categorie = categorie; }
+
+    public String getCategorie() { return categorie; }
+    public String getNationalitate() { return nationalitate; }
+
+    @Override
+    public String toString() {
+        return nume + " " + prenume + " " + nationalitate;
+    }
+}
